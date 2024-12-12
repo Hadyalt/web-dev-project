7745afe9ec98aae23faf4ef1ca3420dbcb1442c4
Jasper85125 <Jaspervanderlaan1105@gmail.com>
2024-12-12 12:30:33 +0100

    Update .gitignore and project paths for user-specific environment changes

--- webdevproject/obj/Debug/net8.0/webdevproject.AssemblyInfo.cs
+++ webdevproject/obj/Debug/net8.0/webdevproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webdevproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d07542b439e68b8317f8ef266c008ca855e4fcc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdfe67f65d37536b521d138f17141bfe32957af8")]
 [assembly: System.Reflection.AssemblyProductAttribute("webdevproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webdevproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
