7a93258689573888aa53b0ab330870f734e1c29c
Jasper85125 <Jaspervanderlaan1105@gmail.com>
2025-01-03 14:31:02 +0100

    add authentication check in Dashboard and Homepage components

--- webdevproject/client/src/Components/Home/Homepage.tsx
+++ webdevproject/client/src/Components/Home/Homepage.tsx
@@ -6,7 +6,7 @@ import { DashboardForm } from "../Dashboard/Dashboard.tsx";
 
 interface HomepageProps {
     backToHome: () => void;
-  }
+}
 
 export class Homepage extends React.Component<HomepageProps, HomepageState> {
     constructor(props: HomepageProps) {
@@ -20,7 +20,16 @@ export class Homepage extends React.Component<HomepageProps, HomepageState> {
     }
 
     componentDidMount() {
-        this.loadEvents();
+        const username = sessionStorage.getItem("username");
+        const userRole = sessionStorage.getItem("userRole");
+
+        if (!username || !userRole) {
+            // Redirect to login page
+            alert("You are not logged in. Redirecting to login page...");
+            window.location.href = "/login";
+        } else {
+            this.loadEvents();
+        }
     }
 
     loadEvents = () => {
