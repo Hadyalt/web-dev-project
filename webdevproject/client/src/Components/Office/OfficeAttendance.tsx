35b56c4ae713a3e721cad646590038b3571fd498
Jasper85125 <Jaspervanderlaan1105@gmail.com>
2025-01-08 14:10:00 +0100

    Remove userId from updatedOffice object in OfficeAttendance component

--- webdevproject/client/src/Components/Office/OfficeAttendance.tsx
+++ webdevproject/client/src/Components/Office/OfficeAttendance.tsx
@@ -49,7 +49,7 @@ export class OfficeAttendance extends React.Component<OfficeAttendanceProps, Off
             return;
         }
 
-        const updatedOffice = { ...selectedOffice, isOccupied: true, userId: Number(sessionStorage.getItem('userId'))};
+        const updatedOffice = { ...selectedOffice, isOccupied: true};
         try {
             await updateOfficeAttendance(updatedOffice);
             this.setState((prevState) => ({
